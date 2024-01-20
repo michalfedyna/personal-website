1a489970b02a14e6bd70d3f8d04b347e7340ffe3
michalfedyna <michal@fedyna.pl>
2024-01-20 16:35:52 +0100

    feat: add fonts

--- lib/personal_web/controllers/page_controller.ex
+++ lib/personal_web/controllers/page_controller.ex
@@ -2,9 +2,11 @@ defmodule PersonalWeb.PageController do
   use PersonalWeb, :controller
 
   def index(conn, _params) do
+    page_title = gettext("Home")
+
     conn
     |> put_layout(html: :app)
-    |> assign(:page_title, "Home")
+    |> assign(:page_title, page_title)
     |> render(:index)
   end
 end
